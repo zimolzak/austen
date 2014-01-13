7eaa61343e02ba3d68d50d00be7494d8f8f6e2a2
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-01-13 00:07:20 -0500

    Allow for strings like "word.--Word"

--- austen.pl
+++ austen.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl -w
+# example: cat *.doc | ./austen.pl | grep -i darcy | grep -i love
 use strict;
 
 local $/; # enable localized slurp mode
@@ -6,9 +7,10 @@ my $content = <>;
 
 $content =~ s/\r\n(\r\n)+/####/g;
 $content =~ s/\r\n/ /g;
-$content =~ s/([.!?])\s+([A-Z])/$1####$2/g; # \s is for Space
-$content =~ s/([.!?]")\s+([A-Z])/$1####$2/g; # end of quote
-$content =~ s/([.!?])\s+(\"[A-Z])/$1####$2/g; # start of quote
+
+$content =~ s/([.!?])[\s-]+([A-Z])/$1####$2/g; # \s is for Space
+$content =~ s/([.!?]")[\s-]+([A-Z])/$1####$2/g; # end of quote
+$content =~ s/([.!?])[\s-]+(\"[A-Z])/$1####$2/g; # start of quote
 
 $content =~ s/([MD]rs?\.)####([A-Z])/$1 $2/g; # Mr. Mrs. Dr.
 
